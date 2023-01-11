bc1e90aa342351532147b17abc836237ce9b4695
Yaroslava Mala <ymala@kse.org.ua>
2023-01-11 22:27:14 +0200

    moving paddle

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ class Paddle:
 
     def __init__(self):
         self.actor = Actor('i.png', center=(300, 765))
+        self.position = Vector(300, 765)
 
     def draw(self):
         self.actor.draw()
 
+    def update(self, pos):
+        self.position = Vector(pos[0], self.position.y)
+        self.actor.x = self.position.x
+
 
 class Background:
+
     def __init__(self):
         self.actor = Actor('bg.jpg', center=(300, 400))
 
@@ -19,20 +25,54 @@ class Background:
         self.actor.draw()
 
 
+class Ball:
+
+    def __init__(self):
+        self.position = Vector(265, 700)
+        self.velocity = Vector(0, 0)  # speed- scalar, velocity - vector
+        self.acceleration = Vector(0, 0)
+
+    def draw(self):
+        screen.draw.filled_circle((self.position.x, self.position.y), 13, "indigo")
+
+    def update(self):
+        pass
+
+
+class Vector:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, 0)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, 0)
+
+
 WIDTH = 600
 HEIGHT = 800
 
 pad = Paddle()
 bg = Background()
+bb = Ball()
 
 
 def draw():
+    screen.clear()
     bg.draw()
     pad.draw()
+    bb.draw()
 
 
 def update(dt):
     pass
+
+
+def on_mouse_move(pos):
+    pad.update(pos)
 
 
 pgzrun.go()
