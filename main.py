25ff9f4606b83e77971c54dd1c697ee79eeeb0d2
Yaroslava Mala <ymala@kse.org.ua>
2023-01-12 21:40:52 +0200

    moving ball

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+
 import pgzrun
 from pgzero.actor import Actor
+
+ball_x_speed = 3
+ball_y_speed = 3
 
 
 class Paddle:
@@ -7,12 +12,18 @@ class Paddle:
     def __init__(self):
         self.actor = Actor('i.png', center=(300, 765))
         self.position = Vector(300, 765)
+        self.center = Vector(62, 765)
 
     def draw(self):
         self.actor.draw()
 
     def update(self, pos):
-        self.position = Vector(pos[0], self.position.y)
+        mouse_x = pos[0]
+        if mouse_x < self.center.x:
+            mouse_x = self.center.x
+        if mouse_x > WIDTH - self.center.x:
+            mouse_x = WIDTH - self.center.x
+        self.position = Vector(mouse_x, self.position.y)
         self.actor.x = self.position.x
 
 
@@ -28,15 +39,28 @@ class Background:
 class Ball:
 
     def __init__(self):
-        self.position = Vector(265, 700)
+        self.position = Vector(265, 630)
         self.velocity = Vector(0, 0)  # speed- scalar, velocity - vector
-        self.acceleration = Vector(0, 0)
+        self.goal = Vector(pad.position.x, pad.position.y)
 
     def draw(self):
         screen.draw.filled_circle((self.position.x, self.position.y), 13, "indigo")
 
     def update(self):
-        pass
+        global ball_x_speed, ball_y_speed
+        self.position.x += ball_x_speed
+        self.position.y += ball_y_speed
+        if (self.position.x >= WIDTH) or (self.position.x <= 0):
+            ball_x_speed *= -1
+        if self.position.y <= 0:
+            ball_y_speed *= -1
+        if self.position.y >= pad.position.y - 5:
+            if pad.position.x - 62 < self.position.x < pad.position.x + 62:
+                ball_x_speed *= -1
+                ball_y_speed *= -1
+            else:
+                time.sleep(2)
+                self.position = Vector(265, 700)
 
 
 class Vector:
@@ -68,7 +92,7 @@ def draw():
 
 
 def update(dt):
-    pass
+    bb.update()
 
 
 def on_mouse_move(pos):
