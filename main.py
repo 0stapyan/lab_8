394ff4ab852f13f058e725f83192b107863f1855
Yaroslava Mala <ymala@kse.org.ua>
2023-01-16 22:01:45 +0200

    add blocks

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import time
 
 import pgzrun
+import pygame
 from pgzero.actor import Actor
+
 
 class Paddle:
 
@@ -11,6 +13,7 @@ class Paddle:
         self.center = Vector(62, 765)
 
     def draw(self):
+        screen.draw.line((0, 60), (600, 60), "indigo")
         self.actor.draw()
 
     def update(self, pos):
@@ -38,8 +41,9 @@ class Ball:
         self.position = Vector(265, 630)
         self.velocity = Vector(0, 0)  # speed- scalar, velocity - vector
         self.goal = Vector(pad.position.x, pad.position.y)
-        self.ball_x_speed = 3
-        self.ball_y_speed = 3
+        self.ball_x_speed = 4
+        self.ball_y_speed = 4
+        self.tries = 3
 
     def draw(self):
         screen.draw.filled_circle((self.position.x, self.position.y), 13, "indigo")
@@ -53,25 +57,62 @@ class Ball:
             self.ball_y_speed *= -1
         if self.position.y >= pad.position.y - 5:
             if pad.position.x - 62 < self.position.x < pad.position.x + 62:
-                #ball_x_speed *= -1
+                # ball_x_speed *= -1
                 self.ball_y_speed *= -1
             else:
-                time.sleep(2)
-                self.position = Vector(265, 700)
+                if self.tries > 0:
+                    self.tries -= 1
+                    time.sleep(0.5)
+                    self.position = Vector(265, 630)
+                else:
+                    time.sleep(2)
+                    self.position = Vector(265, 630)
+
 
 class Health:
 
     def __init__(self, x):
         self.actor = Actor('health.png', center=(x, 30))
         self.position = Vector(x, 30)
-        self.line1 = Vector(0, 60)
-        self.line2 = Vector(600, 60)
 
     def draw(self):
-        screen.draw.line((self.line1.x, self.line1.y), (self.line2.x, self.line2.y), "indigo")
         self.actor.x = self.position.x + 25
         self.actor.y = self.position.y
         self.actor.draw()
+
+
+class Blocks1:
+
+    def __init__(self, x, y):
+        self.position = Vector(x, y)
+
+    def draw(self):
+        screen.draw.filled_circle((self.position.x, self.position.y), 18, "midnightblue")
+
+    def update(self):
+        pass
+
+
+class Blocks2:
+
+    def __init__(self, x, y):
+        self.position = Vector(x, y)
+
+    def draw(self):
+        screen.draw.filled_rect(pygame.Rect((self.position.x, self.position.y, 85, 29)), "crimson")  # x = 25 y = 261
+
+    def update(self):
+        pass
+
+
+class Finish:
+
+    def __init__(self):
+        self.actor = Actor('finish.png', center=(300, 400))
+
+    def draw(self):
+        self.actor.draw()
+
 
 class Vector:
 
@@ -85,25 +126,38 @@ class Vector:
     def __sub__(self, other):
         return Vector(self.x - other.x, 0)
 
+
 WIDTH = 600
 HEIGHT = 800
 
 pad = Paddle()
 bg = Background()
 bb = Ball()
-hh = [Health(x*45) for x in range(3)]
+b1 = [Blocks1(x*45+45, y*45) for x in range(12) for y in range(3, 6)]
+b2 = [Blocks2((x+1)*27+85*x, y*45) for x in range(5) for y in range(6, 9)]
+fin = Finish()
 
 
 def draw():
     screen.clear()
     bg.draw()
     pad.draw()
+    hh = [Health(x*45) for x in range(0, bb.tries)]
     [x.draw() for x in hh]
     bb.draw()
+    [x.draw() for x in b1]
+    [x.draw() for x in b2]
+    if bb.tries == 0:
+        screen.clear()
+        bg.draw()
+        fin.draw()
+        time.sleep(20)
 
 
 def update(dt):
     bb.update()
+    if bb.tries == 0:
+        bb.tries = 3
 
 
 def on_mouse_move(pos):
