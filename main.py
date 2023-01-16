97615e4ef8b937674f82e411d304c47bc21d6308
ostapturash <oturash@kse.org.ua>
2023-01-17 00:43:00 +0200

    Full game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-
+import random
 import pgzrun
 import pygame
 from pgzero.actor import Actor
@@ -38,11 +38,11 @@ class Background:
 class Ball:
 
     def __init__(self):
-        self.position = Vector(265, 630)
+        self.position = Vector(265, 300)
         self.velocity = Vector(0, 0)  # speed- scalar, velocity - vector
         self.goal = Vector(pad.position.x, pad.position.y)
-        self.ball_x_speed = 4
-        self.ball_y_speed = 4
+        self.ball_x_speed = 6
+        self.ball_y_speed = 6
         self.tries = 3
 
     def draw(self):
@@ -63,10 +63,10 @@ class Ball:
                 if self.tries > 0:
                     self.tries -= 1
                     time.sleep(0.5)
-                    self.position = Vector(265, 630)
+                    self.position = Vector(265, 300)
                 else:
                     time.sleep(2)
-                    self.position = Vector(265, 630)
+                    self.position = Vector(265, 300)
 
 
 class Health:
@@ -93,16 +93,16 @@ class Blocks1:
         pass
 
 
-class Blocks2:
-
-    def __init__(self, x, y):
-        self.position = Vector(x, y)
-
-    def draw(self):
-        screen.draw.filled_rect(pygame.Rect((self.position.x, self.position.y, 85, 29)), "crimson")  # x = 25 y = 261
-
-    def update(self):
-        pass
+# class Blocks2:
+#
+#     def __init__(self, x, y):
+#         self.position = Vector(x, y)
+#
+#     def draw(self):
+#         screen.draw.filled_rect(pygame.Rect((self.position.x, self.position.y, 85, 29)), "crimson")  # x = 25 y = 261
+#
+#     def update(self):
+#         pass
 
 
 class Finish:
@@ -113,6 +113,11 @@ class Finish:
     def draw(self):
         self.actor.draw()
 
+class Winmessage:
+    def __init__(self):
+        self.actor = Actor('win.png', center= (300, 400))
+    def draw(self):
+        self.actor.draw()
 
 class Vector:
 
@@ -134,9 +139,9 @@ pad = Paddle()
 bg = Background()
 bb = Ball()
 b1 = [Blocks1(x*45+45, y*45) for x in range(12) for y in range(3, 6)]
-b2 = [Blocks2((x+1)*27+85*x, y*45) for x in range(5) for y in range(6, 9)]
+# b2 = [Blocks2((x+1)*27+85*x, y*45) for x in range(5) for y in range(6, 9)]
 fin = Finish()
-
+win = Winmessage()
 
 def draw():
     screen.clear()
@@ -146,20 +151,27 @@ def draw():
     [x.draw() for x in hh]
     bb.draw()
     [x.draw() for x in b1]
-    [x.draw() for x in b2]
+    # [x.draw() for x in b2]
     if bb.tries == 0:
         screen.clear()
         bg.draw()
         fin.draw()
-        time.sleep(20)
+    if len(b1) == 0:
+        screen.clear()
+        bg.draw()
+        win.draw()
 
 
 def update(dt):
     bb.update()
-    if bb.tries == 0:
-        bb.tries = 3
-
-
+    for obs in b1:
+        if (obs.position.x - 18 < bb.position.x < obs.position.x + 18) \
+                and (obs.position.y -18 < bb.position.y < obs.position.y + 18):
+            b1.remove(obs)
+            bb.ball_y_speed *= -1
+            rand = random.randint(0, 1)
+            if rand:
+                bb.ball_x_speed *= -1
 def on_mouse_move(pos):
     pad.update(pos)
 
